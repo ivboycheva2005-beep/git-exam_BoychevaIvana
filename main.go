5e50260c7d2867438254d20dc334fd4d3c4c0398
Ivana Boycheva <ivana.boycheva@ynov.com>
2025-12-05 14:26:31 +0100

    ex4.etape1

--- main.go
+++ main.go
@@ -10,3 +10,13 @@ func main() {
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Colors</h1><style>body{background-color: #006400;}</style>")
+}
+
+func main() {
+    ...
+    http.HandleFunc("/color", ColorHandler)
+    ...
+}
